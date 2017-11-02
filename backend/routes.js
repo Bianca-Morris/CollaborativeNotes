3d24363a65e1b855782e94f8ed6d9094a3c13cca
Ingrid Zippe <ingrid_zippe@brown.edu>
2017-11-02 13:09:28 -0700

    histories

--- backend/routes.js
+++ backend/routes.js
@@ -37,6 +37,41 @@ router.post('/createdoc', function(req, res) {
   });
 });
 
+router.post('/save/documentID', function(req, res) {
+  console.log("reached /createdoc endpoint...")
+  console.log('REQ.BODY', req.body);
+  Document.find({_id: "59fb66cd58f680350bde1a22"}, function(err, document) {
+    console.log('DOCUMENT', document);
+    var pastHistory = document[0].history;
+    console.log('pastHistory', pastHistory);
+    console.log('req.body.currDocContents', req.body.currDocContents);
+    var newHistory = pastHistory.push(req.body.currDocContents);
+    console.log('newHistory', newHistory);
+  });
+  db.documents.update(
+    {_id: "59fb66cd58f680350bde1a22"},
+    { $set: { "history": newHistory} }
+  )
+  // var doc = new models.Document({
+  //   title: req.body.docTitle,
+  //   owner: req.user._id,
+  //   collaborators: [req.user._id],
+  //   password: '',
+  //   history: [],
+  // });
+  // console.log("attempting to save new document...")
+  // doc.save(function(err, user) {
+  //   if (err) {
+  //     console.log(err);
+  //     res.status(500).json({success: false});
+  //     return;
+  //   } else {
+  //     console.log('Document successfully saved to database.');
+  //     res.status(200).json({success: true, doc: doc});
+  //   }
+  // });
+});
+
 router.get('/fetchuserdocs', function(req, res) {
 
 });
