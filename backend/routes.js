2832075797e48973b1c25760a0cd7f19711f9524
Bianca-Morris <bmorris@wellesley.edu>
2017-11-03 12:20:41 -0700

    resolve merge conflict

--- backend/routes.js
+++ backend/routes.js
@@ -36,7 +36,6 @@ router.post('/createdoc', function(req, res) {
   });
 });
 
-<<<<<<< HEAD
 router.post('/save/documentID', function(req, res) {
   console.log("reached /createdoc endpoint...")
   console.log('REQ.BODY', req.body);
@@ -72,9 +71,7 @@ router.post('/save/documentID', function(req, res) {
   // });
 });
 
-router.get('/fetchuserdocs', function(req, res) {
 
-=======
 router.get('/fetchdocs', function(req, res) {
   console.log("Reached /fetchdocs endpoint");
   console.log("Attempting to retrieve documents...");
@@ -92,7 +89,6 @@ router.get('/fetchdocs', function(req, res) {
        res.status(200).json({success: true, docs: docs, user: req.user.username});
      }
   });
->>>>>>> 6c0c395325b49e7c71a79776b1626591ef53930d
 });
 
 module.exports = router;
